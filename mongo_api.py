8ea97039d588460331c16db1d669df7e27cd3184
Alex <awesome.dynamics@gmail.com>
2018-03-30 22:22:09 +0300

    another one

--- mongo_api.py
+++ mongo_api.py
@@ -2,18 +2,16 @@ from pymongo import MongoClient
 import datetime
 import os
 
-
-#функция апдейтит базу контактом или продуктом
-client = MongoClient(os.environ["MONGODB_URL"], username = os.environ["MONGODB_USERNAME"], password = os.environ["MONGODB_PASSWORD"], authSource = os.environ["MONGODB_AUTHSOURCE"])
+# функция апдейтит базу контактом или продуктом
+client = MongoClient(os.environ["MONGODB_URL"], username=os.environ["MONGODB_USERNAME"],
+                     password=os.environ["MONGODB_PASSWORD"], authSource=os.environ["MONGODB_AUTHSOURCE"])
 db = client[os.environ["MONGODB_AUTHSOURCE"]]
 bookings_coll = db.bookings
 log_coll = db.log
 
 
-def update_booking(chat_id, product = None, contact = None, userstory = None):
-
+def update_booking(chat_id, product=None, contact=None, userstory=None):
     if product is not None:
-
         bookings_coll.update_one(
             {"chat_id": chat_id},
             {
@@ -22,7 +20,6 @@ def update_booking(chat_id, product = None, contact = None, userstory = None):
         )
 
     if contact is not None:
-
         bookings_coll.update_one(
             {"chat_id": chat_id},
             {
@@ -31,7 +28,6 @@ def update_booking(chat_id, product = None, contact = None, userstory = None):
         )
 
     if userstory is not None:
-
         bookings_coll.update_one(
             {"chat_id": chat_id},
             {
@@ -40,31 +36,29 @@ def update_booking(chat_id, product = None, contact = None, userstory = None):
         )
 
 
-#функция пишет лог в базу
+# функция пишет лог в базу
 
-def update_log(chat_id = None, message = None):
-
+def update_log(chat_id=None, message=None):
     if chat_id is not None and message is not None:
         print(message)
         username = str(message.chat.first_name) + " " + str(message.chat.last_name)
 
         log_record = {
-                "chat_id" : message.chat.id,
-                "name" : username,
-                "time" : datetime.datetime.now(),
-                "message" : message.text
-            }
+            "chat_id": message.chat.id,
+            "name": username,
+            "time": datetime.datetime.now(),
+            "message": message.text
+        }
         username = str(message.chat.first_name) + " " + str(message.chat.last_name)
 
         log_coll.insert_one(log_record)
 
-def register_user(message):
 
-    existing_booking = bookings_coll.find_one({"chat_id":message.chat.id})
+def register_user(message):
+    existing_booking = bookings_coll.find_one({"chat_id": message.chat.id})
     print(existing_booking)
 
     if existing_booking is None:
-
         username = str(message.chat.first_name) + " " + str(message.chat.last_name)
 
         booking = {
@@ -76,6 +70,7 @@ def register_user(message):
 
         # вставляем в монго запись - раз и навсегда
         bookings_coll.insert_one(booking)
+
 
 def create_registration(call):
     user_id = call.from_user.id
--- squiz.py
+++ squiz.py
@@ -5,7 +5,7 @@ import datetime
 from flask import Flask, request
 from flask_sslify import SSLify
 import os
-from mongo_api import update_booking, update_log, register_user
+from mongo_api import update_booking, update_log, register_user, create_registration
 
 #подключаемся к монго
 client = MongoClient(os.environ["MONGODB_URL"], username = os.environ["MONGODB_USERNAME"], password = os.environ["MONGODB_PASSWORD"], authSource = os.environ["MONGODB_AUTHSOURCE"])
